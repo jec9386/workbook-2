093ce4fd0a05f0ca1a9840f9caa5f8c00240a223
jec9386 <jerrychandev@gmail.com>
2025-04-21 09:15:57 -0400

    create loop exercise

---
+++ LoopingExercises/src/main/java/com/pluralsight/LoopingExercise.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class LoopingExercise {
+
+    public static void  main(String[] args){
+        int i = 1;
+
+
+        while (i <= 5){
+            System.out.println("I love Java");
+            i++;
+        }
+
+    }
+
+
+}
