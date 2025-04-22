882e4e26396315229f5a480a6e88a444c6f9f619
jec9386 <jerrychandev@gmail.com>
2025-04-22 14:58:41 -0400

    created a new porject and added feilds

---
+++ VehicleInventory/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Vehicle {
+    //no need for main not creating a method but objects
+
+    //variable declarations or instance variables (also called fields).- store state of the object.
+    // These variables are used to store data that is associated with objects of the class.
+    //I made them private so they can;t be accessd directly from outside the class. this term is called Encapsulation
+    
+    private long vehicleId;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private float price;
+
+
+}
+
+
---
+++ VehicleInventory/src/main/java/com/pluralsight/VehicleInventory.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class VehicleInventory {
+    public static void main(String[] args) {
+
+    }
+}
